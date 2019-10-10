a3e7266d9844388a9a696d3793e0beeda06b0340
Vladislav.Sharikov <vladislav.sharikov@billing.ru>
2019-10-10 16:01:46 +0300

    woot(*): angular/cdk overlay NO problem withouot ivy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {OverlayModule} from '@angular/cdk/overlay';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    OverlayModule,
     BrowserModule
   ],
   providers: [],
